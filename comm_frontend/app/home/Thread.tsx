e78d6a7f0a7e2886d347b9c83c17db8bd88256e3
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-09-08 14:58:19 +0200

    made a first visualisation of threads

---
+++ comm_frontend/app/home/Thread.tsx
@@ -0,0 +1,20 @@
+interface Thread {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  date: string;
+  awnsers: string[];
+}
+
+export default function Thread({ thread }: { thread: Thread }) {
+  return (
+    <div className="p-4 rounded-lg bg-green-400 flex flex-col mx-auto w-2/3">
+        
+      <div className="mx-auto flex flex-col">
+        <h1 className="text-2xl font-bold">{thread.title}</h1>
+        <h2>{thread.date}</h2>
+      </div>
+    </div>
+  );
+}
