531c5cb32c0d37724ae64346cf7d706e25f543e5
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-10-27 14:18:34 +0200

    I edited the styling again

--- comm_frontend/app/Thread.tsx
+++ comm_frontend/app/Thread.tsx
@@ -9,8 +9,8 @@ interface Thread {
 
 export default function Thread({ thread }: { thread: Thread }) {
   return (
-    <div className="p-4 rounded-lg bg-green-400 flex flex-col mx-auto w-2/3 lg:w-2/4">
-      <div className="mx-auto flex flex-col">
+    <div className="p-4 rounded-lg bg-green-500 flex mx-auto my-2 w-2/3 lg:w-2/4 ">
+      <div className="mx-auto flex flex-col hover:cursor-pointer">
         <h1 className="text-2xl font-bold">{thread.title}</h1>
         <h2>{thread.date}</h2>
         <span>{'💬' + thread.awnsers.length}</span>
--- comm_frontend/app/layout.tsx
+++ comm_frontend/app/layout.tsx
@@ -17,13 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-24 w-full bg-green-400 fixed flex items-center">
+        <div className="h-24 w-full bg-green-500 fixed flex items-center">
           <h1 className="text-3xl font-bold flex items-center mx-2 mr-10">
             <span className="text-3xl shadow-xl">&#60;</span>
             <span className="text-3xl shadow-xl mx-2">&#62;</span>
             CodeReview
           </h1>
-          <button className="bg-blue-300 rounded-md px-3 pb-1 text-xl font-bold">Home</button>
+          <button className="bg-teal-500 rounded-md px-3 pb-1 text-xl font-bold">
+            Home
+          </button>
         </div>
         {children}
       </body>
