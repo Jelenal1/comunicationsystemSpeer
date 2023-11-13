ce770ccc6dce7a7f4c28ce1ebd9a91c08d8cd1ce
jelenal1 <jabbruh10@gmail.com>
2023-11-13 20:54:09 +0100

    Inorder to make it work

--- comm_frontend/app/threads/[slug]/Newawnser.tsx
+++ comm_frontend/app/threads/[slug]/Newawnser.tsx
@@ -1,29 +1,35 @@
 "use client";
 
+import { useState } from "react";
+
 interface Awnser {
   author: string;
   awnser: string;
 }
-async function postAwnser(id: string, awnser: Awnser) {
+async function postAwnser(id: string, author: string, awnser: string) {
   await fetch(`http://localhost:3000/api/threads/${id}`, {
-    method: "POST",
+    method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ awnser }),
-  });
+    body: JSON.stringify({
+      "author": author,
+      "awnser": awnser,
+    })
+})
 }
 
 export default function Newawnser({ id }: { id: string }) {
+  const [awnser, setAwnser] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
     <div className="p-4 rounded-lg bg-green-500 mx-auto my-2 w-2/4 ">
       <form
         className="flex"
-        onSubmit={(e) => {
+        onSubmit={e => {
           e.preventDefault();
-          const author = (e.target as any).author.value;
-          const awnser = (e.target as any).awnser.value;
-          postAwnser(id, { author, awnser });
+          postAwnser(id, author, awnser);
         }}
       >
         <input
@@ -31,12 +37,14 @@ export default function Newawnser({ id }: { id: string }) {
           name="author"
           placeholder="Author"
           className="mr-2 text-black"
+          onChange={e => setAuthor(e.target.value)}
         />
         <input
           type="text"
           name="awnser"
           placeholder="Awnser"
           className="text-black w-full"
+          onChange={e => setAwnser(e.target.value)}
         />
         <input type="submit" value="Send" className="ml-2" />
       </form>
