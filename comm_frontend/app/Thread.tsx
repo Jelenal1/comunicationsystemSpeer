85f587ca54853a858178c8af707696f535ede808
jelenal1 <jabbruh10@gmail.com>
2023-11-17 20:19:01 +0100

    I try to make it responsive

--- comm_frontend/app/Thread.tsx
+++ comm_frontend/app/Thread.tsx
@@ -13,8 +13,8 @@ export default function Thread({ thread }: { thread: Thread }) {
 	return (
 		<div className="p-4 rounded-lg bg-green-500 grid grid-cols-1 mx-auto my-2 w-2/3 lg:w-2/4 ">
 			<Link href={`/threads/${thread._id}`}>
-				<div className="grid grid-cols-2 hover:cursor-pointer">
-					<div>
+				<div className="grid lg:grid-cols-2 gap-4 hover:cursor-pointer">
+					<div className="flex flex-col">
 						<h1 className="text-2xl font-bold">{thread.title}</h1>
 						<h2>{thread.date}</h2>
 						<span>{"💬" + thread.awnsers.length}</span>
--- comm_frontend/app/layout.tsx
+++ comm_frontend/app/layout.tsx
@@ -1,34 +1,38 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'CodeReview',
-  description: 'A code review app',
+	title: "CodeReview",
+	description: "A code review app",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="h-24 w-full bg-green-500 fixed flex items-center">
-          <h1 className="text-3xl font-bold flex items-center mx-2 mr-10">
-            <span className="text-3xl shadow-xl">&#60;</span>
-            <span className="text-3xl shadow-xl mx-2">&#62;</span>
-            CodeReview
-          </h1>
-          <button className="bg-teal-500 rounded-md px-3 pb-1 text-xl font-bold">
-            Home
-          </button>
-        </div>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<div className="h-24 w-full bg-green-500 fixed flex items-center">
+					<h1 className="text-3xl font-bold flex items-center mx-2">
+						<span className="text-3xl shadow-xl">&#60;</span>
+						<span className="text-3xl shadow-xl mx-2">&#62;</span>
+						CodeReview
+					</h1>
+					<Link
+						href={"/"}
+						className="bg-teal-500 rounded-md px-3 mt-1 pb-1 text-xl font-bold"
+					>
+						Home
+					</Link>
+				</div>
+				{children}
+			</body>
+		</html>
+	);
 }
